2e9e9bf54755301f34e8026efbb6729eebf5b169
kosiarn <62908964+kosiarn@users.noreply.github.com>
2024-01-04 15:01:25 +0100

    add cryptography module

---
+++ cryptography.js
@@ -0,0 +1,10 @@
+//TODO implement password hashing
+
+const mockHashPassword = (password) => {
+    return password;
+}
+
+// change hashing function name here after you implement it
+const hashingAlgorithm = mockHashPassword;
+
+module.exports = {hashingAlgorithm}
