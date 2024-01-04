abebb35ccf304c8c4520108a78768d1b5eda621d
kosiarn <62908964+kosiarn@users.noreply.github.com>
2024-01-04 15:21:17 +0100

    add login parameters validation

--- db_connection.js
+++ db_connection.js
@@ -1,6 +1,22 @@
 const {db} = require('./db_init');
 
+const validateLoginParameter = (parameter) => {
+    //checks if passed login parameter is valid.
+    invalidParameterPassed = parameter === null || parameter === '' || parameter.includes(' ');
+    if(invalidParameterPassed){
+        return false
+    }
+    return true
+}
+
+const validateLoginParameters = (login, password) => {
+    if(!validateLoginParameter(login) || !validateLoginParameter(password)){
+        throw new Error("Login parameters are invalid.")
+    }
+}
+
 const validateUser = async (login, password) => {
+    validateLoginParameters(login, password)
     // retrieves user identity from database.
     var fetched_user = null;
     db.get("SELECT * from Users WHERE username = $login", {$login: login}, (err, usr) => {
@@ -15,6 +31,7 @@ const validateUser = async (login, password) => {
 }
 
 const registerUser = async (login, password) => {
+    validateLoginParameters(login, password)
     db.run("INSERT INTO Users (username, password) VALUES(?)", [login, password], (err) => {
         if(err){
             throw new Error(err)
