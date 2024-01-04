4428a048d06cccef277259beec86a890b102f0a2
kosiarn <kosiarn@users.noreply.github.com>
2024-01-04 19:01:51 +0100

    add Passport config

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const  Session =  require("express-session");
 const  Passport =  require("passport");
 const {APPLICATION_PORT} =  require('./config');
+const db_ops = require('./db_connection')
 
 // Init step
 const app = express();
@@ -9,7 +10,24 @@ app.use(Session({secret: 'sso_SECRET_key', resave: true, saveUninitialized: true
 app.use(Passport.initialize());
 app.use(Passport.session());
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
 
+passport.deserializeUser((id, done) => {
+    const user = db_ops.getUserByID(id);
+    done(null, user);
+});
+
+passport.use(new LocalStrategy(
+    (username, password, done) => {
+      const user = db_ops.validateUser(username, password);
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username or password' });
+      }
+      return done(null, user);
+    }
+  ));
 
 // API routes
 app.get('/', (req, res) => {
