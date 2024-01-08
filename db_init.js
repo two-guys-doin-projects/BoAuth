c28ce1c86514aa2126a0a3aab1a7bb00ebbe92ee
kosiarn <62908964+kosiarn@users.noreply.github.com>
2024-01-08 11:14:22 +0100

    add console log on creating SQLite database

--- db_init.js
+++ db_init.js
@@ -8,8 +8,8 @@ const localDB = () => {
     const db = new sqlite3.Database(USER_IDENTITY_DB_FILE_PATH);
     
     if(!userDBExists){
+        console.log(`Database file created at ${USER_IDENTITY_DB_FILE_PATH}; running migration...`)
         db.run("CREATE TABLE Users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT); COMMIT;");
-        
     }
     return db
 }
