184df42f4364d1140924ace144c2285cdfa223e9
kosiarn <kosiarn@users.noreply.github.com>
2024-01-08 21:06:46 +0100

    add auth route

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const  Session =  require("express-session");
 const  Passport =  require("passport");
-const {APPLICATION_PORT} =  require('./config');
+const {APPLICATION_PORT, JWT_KEY} =  require('./config');
 const db_ops = require('./db_connection')
+const jwt = require('jsonwebtoken')
 
 // Init step
 const app = express();
@@ -34,11 +35,31 @@ app.get('/', (req, res) => {
     res.send('Welcome')
 })
 
-app.post('/login', passport.authenticate('local', {
-    successRedirect: '/dashboard',
-    failureRedirect: '/login',
-    failureFlash: true
-  }));
+app.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user) => {
+      if (err || !user) {
+        // Handle authentication failure
+        return res.redirect('/login-page');
+      }
+  
+      // Assuming you have some property like 'returnTo' in the request
+      const redirectTo = req.body.returnTo || '/login';
+  
+      // Issue a JWT or perform any other necessary actions
+      
+      const user_identity = {
+        id: `boauth_${user.id}`,
+        name: user.username
+      }
+
+      const user_token = jwt.sign(user_identity, JWT_KEY)
+
+      // Redirect the user to the specified destination
+      return res.redirect(redirectTo + `?token=${user_token}`);
+    })(req, res, next);
+  });
+
+
 
 // API bootstrap
 app.listen(APPLICATION_PORT, () => {
