0e06e7ebf775d9f747c84cecdbf4975d3af04dcf
kosiarn <62908964+kosiarn@users.noreply.github.com>
2024-01-08 11:12:29 +0100

    move SQLite database init into a function ...to make it more like dependency injectionish

--- db_init.js
+++ db_init.js
@@ -2,13 +2,18 @@ const fs = require('fs');
 const {USER_IDENTITY_DB_FILE_PATH} = require('./config');
 const sqlite3 = require('sqlite3');
 
-const userDBExists = fs.existsSync(USER_IDENTITY_DB_FILE_PATH);
+const localDB = () => {
+    const userDBExists = fs.existsSync(USER_IDENTITY_DB_FILE_PATH);
 
-const db = new sqlite3.Database(USER_IDENTITY_DB_FILE_PATH);
-
-if(!userDBExists){
-    db.run("CREATE TABLE Users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT); COMMIT;");
+    const db = new sqlite3.Database(USER_IDENTITY_DB_FILE_PATH);
     
+    if(!userDBExists){
+        db.run("CREATE TABLE Users (id INTEGER PRIMARY KEY, username TEXT UNIQUE, password TEXT); COMMIT;");
+        
+    }
+    return db
 }
+
+const db = localDB()
 
 module.exports = {db}
